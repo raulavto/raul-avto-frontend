01e46a14d8c4984ada1ad0ac800e880cf2e1c0dc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-27 18:49:53 +0200

    added prizes section

--- src/components/Competition/Prizes/PrizesList.tsx
+++ src/components/Competition/Prizes/PrizesList.tsx
@@ -1,18 +1,19 @@
+import PrizeItem from './PrizeItem';
+
 export default function PrizesList() {
   const prizesList = [
     {
       number: '#1',
-      image: 'auto',
+      image: 'car',
       title: 'Audi A8 2014 Long 4.0 TFSI V8 (Stage 1)',
       description:
-        'Audi A8 2014 Long 4.0 TFSI V8 – розкіш і потужність в твоїх руках!',
+        'Твоя мрія — Audi A8 2014 Long 4.0 TFSI V8! Розкіш та потужність у кожному русі!',
     },
     {
       number: '#2',
       image: 'phone',
       title: 'iPhone 16',
-      description:
-        'Твоя мрія — Audi A8 2014 Long 4.0 TFSI V8! Розкіш та потужність у кожному русі!',
+      description: 'Виграй новий iPhone 16 — стиль та інновації в твоїх руках!',
     },
     {
       number: '#3',
@@ -22,5 +23,24 @@ export default function PrizesList() {
     },
   ];
 
-  return <ul>PrizesList</ul>;
+  return (
+    <ul
+      className="flex flex-col pointuserbar:flex-row pointuserbar:justify-between pointuserbar:items-center gap-y-10 
+    pointuserbar:gap-x-8 mac:gap-x-12 max-w-[430px] pointuserbar:max-w-[1215px] mx-auto text-white"
+    >
+      {prizesList.map((prize, idx) => (
+        <PrizeItem
+          key={idx}
+          prize={prize}
+          className={`${
+            idx === 0
+              ? 'w-[164px] pointuserbar:w-[240px] mac:w-[380px] pointuserbar:h-[125px] h-[85px] mac:h-[197px]'
+              : idx === 1
+              ? 'w-[98px] pointuserbar:w-[136px] h-[98px] pointuserbar:h-[136px]'
+              : 'w-[130px] pointuserbar:w-[222px] h-[91px] pointuserbar:h-[130px]'
+          }`}
+        />
+      ))}
+    </ul>
+  );
 }

4ec43a3f547a0b610b48b3859f0ea85497b0246f
AnnaFutryk <anna.futryk@gmail.com>
2025-03-29 14:51:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,11 @@ export default function RootLayout({
   return (
     <html lang="uk">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+
         <GoogleTagManager gtmId="GTM-MM6J6JCP" />
         <Suspense fallback={null}>
           <FacebookPixelEvents />

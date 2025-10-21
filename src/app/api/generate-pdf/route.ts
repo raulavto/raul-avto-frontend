5120e2cb4c6ee37819aa2d7cb290c68964aa4ae8
Shade <kulakaa073@gmail.com>
2025-10-21 10:10:03 +0300

    fix: fixing pdf route

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: NextRequest) {
     });
 
     // Navigate to the PDF template route
-    const baseUrl = 'https://www.raul-avto.com/';
+    const baseUrl = 'https://www.raul-avto.com';
 
     const url = `${baseUrl}/api/pdf-template?data=${encodeURIComponent(
       JSON.stringify(data)
